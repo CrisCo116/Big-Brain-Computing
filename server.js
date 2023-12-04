b27fbb3c69b1f718be3229684ca4da0850c1d6e1
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-12-03 21:47:07 -0800

    added in javascript to the inquire form.

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config(); // Load environment variables from a .env file
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Serve static files (e.g., HTML, CSS)
+app.use(express.static('public'));
+
+// Send email upon form submission
+app.post('/submit-form', (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Replace with your email configuration using environment variables
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: '3nwllc@gmail.com', // Replace with your company's email
+    subject: 'New Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
