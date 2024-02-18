d4742ef2a098789a498b432e1fb1d63bfc89276f
Chris <138630942+CrisCo116@users.noreply.github.com>
2024-02-17 20:21:39 -0800

    new changes

--- JS/inquire.js
+++ JS/inquire.js
@@ -1,22 +1,21 @@
-// Initialize EmailJS
-(function() {
-    // Replace 'YOUR_PUBLIC_KEY' with your actual public key
+// Initialize EmailJS after the HTML has been loaded
+document.addEventListener('DOMContentLoaded', function() {
     emailjs.init({
         publicKey: "n-Ts8f5T3OVM86shH",
     });
-})();
 
-// Add event listener to handle form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    // Add event listener to handle form submission
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
 
-    // Send the form data using EmailJS
-    emailjs.sendForm('contact_service', 'contact_form', this)
-        .then(() => {
-            console.log('SUCCESS!'); // Log success message
-            toggleFormVisibility(); // Hide the form after successful submission
-        }, (error) => {
-            console.log('FAILED...', error); // Log error message
-            // You can handle the error here if needed
-        });
+        // Send the form data using EmailJS
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(() => {
+                console.log('SUCCESS!'); // Log success message
+                toggleFormVisibility(); // Hide the form after successful submission
+            }, (error) => {
+                console.log('FAILED...', error); // Log error message
+                // You can handle the error here if needed
+            });
+    });
 });
